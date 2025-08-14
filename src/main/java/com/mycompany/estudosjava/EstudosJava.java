b7cad7b2c02620c048108027da968bbf856e249e
Barbara <barbara.bastos1995@gmail.com>
2025-08-13 22:03:40 -0300

    ola

---
+++ src/main/java/com/mycompany/estudosjava/EstudosJava.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.estudosjava;
+
+/**
+ *
+ * @author usuario
+ */
+public class EstudosJava {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
